9cbed6e104427ccbd9cda5032f5373924cad88cf
tyaslab <tyaslab.com@gmail.com>
2014-06-28 10:18:27 +0700

    update the module, add some events

--- upload.js
+++ upload.js
@@ -1,7 +1,6 @@
 /* USAGE
  * 
  * <div id="upload-{{ $index }}" data-tyaslab-upload
- *      data-ng-model="object.image"
  *      data-name="filename"
  *      data-action="/upload/image/"
  *      data-accept="image/*">
@@ -9,38 +8,42 @@
  * 
  */
 
-// TODO : onSuccess, onFail, onProgress
-
 angular.module('tyaslab.upload', [])
-.directive('tyaslabUpload', function($parse) {
+.directive('tyaslabUpload', ['$parse', '$rootScope', function($parse, $rootScope) {
     return {
         restrict : 'A',
         template :
             '<div class="fakeUpload">' +
                 '<input name="{{ name }}" accept="{{ accept }}" data-action="{{ action }}" type="file">' +
-                '<a class="fakeLink" href="javascript:void(0);"><span ng-transclude></span></a>' +
+                '<span ng-transclude class="fakeLink"></span>' +
             '</div>',
         transclude : true,
         scope : {
             id : '@',
             ngModel : '=',
+            index : '@',
             action : '@',
             accept : '@',
             name : '@'
         },
         link : function(scope, element, attrs, ctrls) {
-            element.find('a').on('click', function() {
-                element.find('[type=file]').click();
+            scope.$watch('index', function(newVal, oldVal) {
+                scope.index = parseInt(newVal);
+            });
+
+            element.find('.fakeLink').bind('click tap', function() {
+                element.find('[type=file]').trigger('click');
             });
             
             scope.$watch('id', function(newVal, oldVal) {
                 scope.id = newVal;
                 
                 document.getElementById(scope.id).getElementsByTagName('input')[0].onchange = function(evt) {
-                    var file = this.files[0];
-                    var formdata = new FormData();
-                    var name = this.getAttribute('name');
-                    var action = this.getAttribute('data-action');
+                    var thisElement = angular.element(this).closest('.fakeUpload'),
+                        file = this.files[0],
+                        formdata = new FormData(),
+                        name = this.getAttribute('name'),
+                        action = this.getAttribute('data-action');
                     
                     formdata.append(name, file);
                     // TODO: USE NATIVE JAVASCRIPT
@@ -52,9 +55,20 @@ angular.module('tyaslab.upload', [])
                         contentType : false,
                         success : function(responseText) {
                             scope.$apply(function() {
+                                $rootScope.$broadcast('upload_success', responseText);
                                 if (attrs.ngModel) {
                                     $parse(attrs.ngModel).assign(scope.$parent, responseText);
                                 }
+                            });
+                        },
+                        beforeSend : function() {
+                            scope.$apply(function() {
+                                $rootScope.$broadcast('upload_before_send', thisElement);
+                            });
+                        },
+                        complete : function() {
+                            scope.$apply(function() {
+                                $rootScope.$broadcast('upload_complete', thisElement);
                             });
                         }
                     });
@@ -62,4 +76,18 @@ angular.module('tyaslab.upload', [])
             });
         }
     };
-});
+}])
+
+.run(['$rootScope', function($rootScope) {
+    $rootScope.$on('upload_before_send', function(evt, element) {
+        console.log('Upload is going up');
+    });
+
+    $rootScope.$on('upload_complete', function(evt, element) {
+        console.log('Upload complete');
+    });
+
+    $rootScope.$on('upload_success', function(evt, responseText) {
+        console.log('File upload succeed resulting: ' + responseText);
+    });
+}]);
